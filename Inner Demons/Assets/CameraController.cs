1b90dfa76e52a42c45815ea82afaf1041b7b3961
Vernixs <enecelis2021@elam.co.uk>
2022-03-29 12:44:22 +0100

    making collisions work well

--- Inner Demons/Assets/CameraController.cs
+++ Inner Demons/Assets/CameraController.cs
@@ -11,11 +11,12 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         
-    }
+     }
 
     // Update is called once per frame
     void Update()
     {
+
         targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
         transform.position = Vector3.Lerp (transform.position, targetPos, moveSpeed * Time.deltaTime);
     }
--- Inner Demons/Assets/PlayerController.cs
+++ Inner Demons/Assets/PlayerController.cs
@@ -6,21 +6,33 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
 
+    Rigidbody2D myRigidbody;
+
+    bool playerMoving;
+    Vector2 lastMove;
+
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
 
     void Update()
     {
+
+        playerMoving = false;
+
         if(Input.GetAxisRaw("Horizontal") > 0.5f  || Input.GetAxisRaw("Horizontal") < -0.5f )
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            //playerMoving = true;
+            //lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
         if(Input.GetAxisRaw("Vertical") > 0.5f  || Input.GetAxisRaw("Vertical") < -0.5f )
         {
             transform.Translate(new Vector3(0F, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            ///playerMoving = true;
+            //lastMove = new Vector2(Input.GetAxisRaw("Vertical"), 0f);
         }
     }
 }
