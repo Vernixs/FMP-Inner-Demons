faed30a7cbf65e95ddaef9755aa07880c6d0e2d7
Vernixs <enecelis2021@elam.co.uk>
2022-03-29 15:02:41 +0100

    Collision almost done

--- Inner Demons/Assets/PlayerController.cs
+++ Inner Demons/Assets/PlayerController.cs
@@ -25,14 +25,26 @@ public class PlayerController : MonoBehaviour
         if(Input.GetAxisRaw("Horizontal") > 0.5f  || Input.GetAxisRaw("Horizontal") < -0.5f )
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
             //playerMoving = true;
             //lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
         if(Input.GetAxisRaw("Vertical") > 0.5f  || Input.GetAxisRaw("Vertical") < -0.5f )
         {
             transform.Translate(new Vector3(0F, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
             ///playerMoving = true;
             //lastMove = new Vector2(Input.GetAxisRaw("Vertical"), 0f);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
         }
     }
 }
