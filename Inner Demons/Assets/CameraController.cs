07d626495e5fb77e827f1517e17d034340d820a8
Vernixs <enecelis2021@elam.co.uk>
2022-03-29 11:52:09 +0100

    camera movement done

---
+++ Inner Demons/Assets/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject followTarget;
+    Vector3 targetPos;
+    public float moveSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp (transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+}
