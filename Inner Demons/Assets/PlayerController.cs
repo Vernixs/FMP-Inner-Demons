8e9d93f6485182a221a169c16392de7dd1fc9bec
Vernixs <enecelis2021@elam.co.uk>
2022-03-30 10:43:47 +0100

    Animations

--- Inner Demons/Assets/PlayerController.cs
+++ Inner Demons/Assets/PlayerController.cs
@@ -6,38 +6,41 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
 
-    Rigidbody2D myRigidbody;
+    private Animator anim;
+
+    /*Rigidbody2D myRigidbody;
 
     bool playerMoving;
-    Vector2 lastMove;
+    Vector2 lastMove;*/
 
     void Start()
     {
-        myRigidbody = GetComponent<Rigidbody2D>();
+        /*myRigidbody = GetComponent<Rigidbody2D>();*/
+        anim = GetComponent<Animator>();
     }
 
 
     void Update()
     {
 
-        playerMoving = false;
+        /*playerMoving = false;*/
 
         if(Input.GetAxisRaw("Horizontal") > 0.5f  || Input.GetAxisRaw("Horizontal") < -0.5f )
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-            myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
-            //playerMoving = true;
-            //lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+           /* myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
+            playerMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);*/
         }
         if(Input.GetAxisRaw("Vertical") > 0.5f  || Input.GetAxisRaw("Vertical") < -0.5f )
         {
             transform.Translate(new Vector3(0F, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
-            ///playerMoving = true;
-            //lastMove = new Vector2(Input.GetAxisRaw("Vertical"), 0f);
+            /*myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+            playerMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Vertical"), 0f);*/
         }
 
-        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        /*if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
         {
             myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
         }
@@ -45,6 +48,9 @@ public class PlayerController : MonoBehaviour
         if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
         {
             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
-        }
+        }*/
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
     }
 }
