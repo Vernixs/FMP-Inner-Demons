c67646e01d9b7b1648f6633f1df73bacf7d5b13c
Vernixs <enecelis2021@elam.co.uk>
2022-03-30 12:40:07 +0100

    Level map testing

--- Inner Demons/Assets/PlayerController.cs
+++ Inner Demons/Assets/PlayerController.cs
@@ -7,11 +7,13 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
 
     private Animator anim;
-
-    /*Rigidbody2D myRigidbody;
-
     bool playerMoving;
-    Vector2 lastMove;*/
+    Vector2 lastMove;
+
+
+    Rigidbody2D myRigidbody;
+
+
 
     void Start()
     {
@@ -23,21 +25,21 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-        /*playerMoving = false;*/
+        playerMoving = false;
 
         if(Input.GetAxisRaw("Horizontal") > 0.5f  || Input.GetAxisRaw("Horizontal") < -0.5f )
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-           /* myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
+            //myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
             playerMoving = true;
-            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);*/
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
         if(Input.GetAxisRaw("Vertical") > 0.5f  || Input.GetAxisRaw("Vertical") < -0.5f )
         {
             transform.Translate(new Vector3(0F, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-            /*myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+            //myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
             playerMoving = true;
-            lastMove = new Vector2(Input.GetAxisRaw("Vertical"), 0f);*/
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
 
         /*if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
@@ -52,5 +54,8 @@ public class PlayerController : MonoBehaviour
 
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
     }
 }
