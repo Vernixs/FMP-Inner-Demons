d8d33507612d7957be767ee05dcfc4559fb3dc51
Vernixs <enecelis2021@elam.co.uk>
2022-04-20 12:40:20 +0100

    level map 2.0

--- Inner Demons/Assets/DialogueManager.cs
+++ Inner Demons/Assets/DialogueManager.cs
@@ -20,7 +20,7 @@ public class DialogueManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        dText.text  = dialogLines[currentLine]
+       // dText.text  = dialogLines[currentLine]
     }
 
     public void ShowBox(string dialogue)
