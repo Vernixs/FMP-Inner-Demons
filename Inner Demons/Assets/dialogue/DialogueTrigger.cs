3af5c671c47dd90b6dcd522e5ad77684534e3b79
Vernixs <enecelis2021@elam.co.uk>
2022-05-19 15:01:50 +0100

    level maps imported

--- Inner Demons/Assets/dialogue/DialogueTrigger.cs
+++ Inner Demons/Assets/dialogue/DialogueTrigger.cs
@@ -9,7 +9,7 @@ public class DialogueTrigger : MonoBehaviour
 
     [Header("Ink JSON")]
     [SerializeField] private TextAsset inkJSON;
-    [SerializeField] private TextAsset inkJSON2;
+
 
     bool dialoguePanel = false;
 
